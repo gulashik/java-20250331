5d65906e9d5dbc8b2df801c2aa045a7b5624ebdb
Максим Гулько <gulashmail@gmail.com>
2025-05-11 17:53:46 +0300

    hw07 #fix5 adding javadoc

--- src/main/java/ru/otus/hw/hw07/entity/transport/implementation/AllTerrainVehicle.java
+++ src/main/java/ru/otus/hw/hw07/entity/transport/implementation/AllTerrainVehicle.java
@@ -9,12 +9,25 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Класс Вездеход
+ * Класс Вездеход - представляет транспортное средство, способное передвигаться по любым типам местности.
+ * 
+ * <p>Особенности вездехода:</p>
+ * <ul>
+ *     <li>Может перемещаться по любой местности</li>
+ *     <li>Имеет повышенный расход топлива при перемещении по труднопроходимым типам местности</li>
+ * </ul>
  */
 public class AllTerrainVehicle extends TerrainMovementConsumption implements Transport {
     @Getter
     private final String name;
 
+    /**
+     * Создает новый экземпляр вездехода с указанными параметрами.
+     *
+     * @param name имя вездехода для идентификации
+     * @param initialFuel начальный запас топлива в литрах
+     * @param fuelConsumption базовый расход топлива (литров на км)
+     */
     public AllTerrainVehicle(String name, double initialFuel, double fuelConsumption) {
         super(
             initialFuel,
@@ -26,6 +39,14 @@ public class AllTerrainVehicle extends TerrainMovementConsumption implements Tra
         this.name = name;
     }
 
+    /**
+     * Перемещает вездеход на указанное расстояние по заданному типу местности.
+     *
+     * @param distance расстояние для перемещения в км
+     * @param terrain тип местности для перемещения
+     * @return {@code true} если перемещение было успешным (достаточно топлива),
+     *         {@code false} в противном случае
+     */
     @Override
     public boolean move(double distance, TerrainType terrain) {
         boolean moved = super.move(distance, terrain);
@@ -39,12 +60,24 @@ public class AllTerrainVehicle extends TerrainMovementConsumption implements Tra
         return moved;
     }
 
+    /**
+     * Проверяет, может ли вездеход передвигаться по указанному типу местности.
+     * Вездеход способен перемещаться по любой местности.
+     *
+     * @param terrain тип местности для проверки
+     * @return {@code true} всегда, так как вездеход может перемещаться по любой местности
+     */
     @Override
     public boolean cantMoveOn(TerrainType terrain) {
         // Вездеход может перемещаться по любой местности
         return true;
     }
 
+    /**
+     * Заправляет вездеход указанным количеством топлива.
+     *
+     * @param amount количество топлива для заправки в литрах
+     */
     public void refuel(double amount) {
         currentResourceValue += amount;
         System.out.println("Вездеход " + name + " заправлен. Текущий запас топлива: " + currentResourceValue + " л.");
--- src/main/java/ru/otus/hw/hw07/entity/transport/implementation/Bicycle.java
+++ src/main/java/ru/otus/hw/hw07/entity/transport/implementation/Bicycle.java
@@ -7,16 +7,37 @@ import ru.otus.hw.hw07.entity.transport.Transport;
 
 import java.util.List;
 
-/** Класс Велосипед */
+/**
+ * Класс Велосипед представляет собой транспортное средство,
+ * которое может передвигаться по определённым типам местности не расходуя энергии.
+ * 
+ * @see Transport
+ * @see TerrainMovementLimit
+ * @see TerrainType
+ */
 public class Bicycle extends TerrainMovementLimit implements Transport {
+
     @Getter
     private final String name;
 
+    /**
+     * Создает новый экземпляр велосипеда с указанным названием.
+     * По умолчанию велосипед не может передвигаться по болоту.
+     * 
+     * @param name название велосипеда
+     */
     public Bicycle(String name) {
         super(List.of(TerrainType.SWAMP));
         this.name = name;
     }
 
+    /**
+     * Перемещает велосипед на указанное расстояние по заданной местности.
+     * 
+     * @param distance расстояние в километрах, которое нужно преодолеть
+     * @param terrain тип местности, по которой происходит перемещение
+     * @return true, если перемещение успешно выполнено, false - если перемещение невозможно
+     */
     @Override
     public boolean move(double distance, TerrainType terrain) {
         if (cantMoveOn(terrain)) {
