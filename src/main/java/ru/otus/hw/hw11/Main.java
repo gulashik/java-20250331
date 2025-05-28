93f48b43ae91dc2a933acccf8fc1c9bbf1cef5a2
Максим Гулько <gulashmail@gmail.com>
2025-05-28 23:15:40 +0300

    hw11 mvp

--- src/main/java/ru/otus/hw/hw11/Main.java
+++ src/main/java/ru/otus/hw/hw11/Main.java
@@ -1,7 +1,17 @@
 package ru.otus.hw.hw11;
 
+import ru.otus.hw.hw11.tree.SearchTreeImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        SearchTreeImpl<Integer> tree = new SearchTreeImpl<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        System.out.println("Поиск элемента 5: " + tree.find(5));
+        System.out.println("Поиск элемента 11: " + tree.find(11));
+
+        System.out.println("Отсортированный список из дерева: " + tree.getSortedList());
     }
 }
